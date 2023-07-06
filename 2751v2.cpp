cbeed1b2bc487ce857da8858c971bc10ecdae89c
나세빈 <94737768+sebbbin@users.noreply.github.com>
2023-07-06 23:00:43 +0900

    [2751] 수 정렬하기 2 실버 5
    endl쓰면 시간초과난다

---
+++ 2751v2.cpp
@@ -0,0 +1,20 @@
+//수 정렬하기 2
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main (){
+    int N;
+    cin >> N;
+    int arr[N];
+    for (int i=0; i<N; i++){
+        cin >>arr[i];
+    }
+    sort(arr, arr+N);
+    for (int i=0; i<N; i++){
+        cout<<arr[i];
+    }
+    return 0;
+}

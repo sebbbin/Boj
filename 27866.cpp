86bae6837d76373175f0989185eb4a4a66fe9fc3
나세빈 <94737768+sebbbin@users.noreply.github.com>
2023-07-21 15:53:45 +0900

    [27866] 문자와 문자열 브론즈 5
    class 1+ 완료

---
+++ 27866.cpp
@@ -0,0 +1,15 @@
+//문자와 문자열 브론즈 5
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main (){
+    string str;
+    cin >> str;
+    int N;
+    cin >>N;
+cout << str[N-1];
+
+}

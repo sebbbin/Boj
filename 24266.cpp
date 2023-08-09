9a75b65d1c527071335be311b248f55ca51af50d
나세빈 <94737768+sebbbin@users.noreply.github.com>
2023-08-09 23:45:15 +0900

    [24266] 알고리즘 수업 - 알고리즘의 수행 시간 5 브론즈 3

---
+++ 24266.cpp
@@ -0,0 +1,14 @@
+//알고리즘 수업 - 알고리즘의 수행 시간 5 브론즈3
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    long long int n;
+    cin >> n; 
+ 
+    cout << n*n*n << '\n' << 3 << endl; 
+ 
+    return 0;
+}

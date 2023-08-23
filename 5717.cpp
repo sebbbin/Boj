fcf6b06c899cd6fb56ef3e9410a7d2da68d6236f
나세빈 <94737768+sebbbin@users.noreply.github.com>
2023-08-23 22:47:12 +0900

    [5717] 상근이의 친구들 브론즈 4

---
+++ 5717.cpp
@@ -0,0 +1,13 @@
+//상근이의 친구들 브론즈 4
+#include <iostream>
+using namespace std;
+
+int main (){
+    int M,F;
+    while(1){
+        cin>>M>>F;
+	if(M==0&&F==0)break;
+        cout<<M+F<<"\n";
+    }
+    return 0;
+}

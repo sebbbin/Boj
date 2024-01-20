ce69f035604d1752d8eefdb7f276ec7abe208b1d
나세빈 <94737768+sebbbin@users.noreply.github.com>
2024-01-20 19:50:33 +0900

    [1402] 아무래도이문제는A번난이도인것같다

--- 1402.cpp
+++ 1402.cpp
@@ -9,20 +9,9 @@ cin.tie(nullptr); cout.tie(nullptr);
 int t;
 cin >> t;
 int a, b;
-int r1, r2;
 while(t--){
 cin >> a >> b;
-for(int i=1; i<b/2+1; i++){
-r1 = i;
-r2 = b-r1;
-if(a == r1*r2){
 cout << "yes"<<"\n";
-break;
-}
-if(i==b/2){
-    cout << "no"<<"\n";
-}
-}
 }
 
 return 0;
